55f19e9a23bdaafe80630f546e0a80a95d0bf45e
WalatH <93122449+WalatH@users.noreply.github.com>
2022-09-23 08:12:12 +0000

    bal stuiter

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+var x, y, vx, vy;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+	createCanvas(1800, 900);
+
+  x = 0;
+  y = 150;
+  vx = 6;
+  vy = 6;
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+	background(0);
+  
+  ellipse(x,y,50,50);
+  x = x + vx;
+  y = y + vy;
+
+  if(x <= 0 || x >= width){
+    vx = vx * -1;
+  }
+
+   if(y <= 0 || y >= height){
+    vy = vy * -1;
+  }
 }
