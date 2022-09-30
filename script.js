aa93271c5474d66cf1e857ffcf857ba3d7102320
WalatH <93122449+WalatH@users.noreply.github.com>
2022-09-30 07:44:08 +0000

    Bouncing ball nog geen collision

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
+
 let movingRight = false;
 let movingLeft = false;
 
-let xpos = 500;
+let xpos = 800;
 let ypos = 700;
-let speed = 5;
+let speed = 6;
 
 var x, y, vx, vy;
 
 function setup() {
-	createCanvas(1000, 900);
+    createCanvas(1000, 900);
 
-  x = 0;
-  y = 150;
-  vx = 6;
-  vy = 6;
+    x = 0;
+    y = 150;
+    vx = 6;
+    vy = 6;
 }
 
 function draw() {
-	background(0);
-    
-  fill(0, 0, 255);
-  rect(xpos, ypos, 100, 25);
-    
+    background(0);
+
+      ellipse(x,y,50,50);
+  x = x + vx;
+  y = y + vy;
+
+  if(x < 0 || x > 1000){
+    vx = vx * -1;
+  }
+  
+  if(y < 0 || y > 900){
+    vy = vy * -1;
+  }
+    fill(0, 0, 255);
+    rect(xpos, ypos, 100, 25);
+
     if (movingRight) {
         xpos += speed;
     }
